5e4603a1cba41a2749a0b42341fda3b039a9e4f6
Tadz1k <50612974+Tadz1k@users.noreply.github.com>
2022-06-15 09:33:28 +0200

    Update distracted.py

--- tools/distracted.py
+++ tools/distracted.py
@@ -17,9 +17,16 @@ states = { "c0": "Bezpieczna jazda",
 "c8": "Makeup",
 "c9": "Rozmowa z pasażerem" }
 
-model_pkl = torch.load('../models/distracted_model.pkl')
-video = cv2.VideoCapture('../data/distracted.mp4')
+def current_milli_time():
+    return round(time.time() * 1000)
+
+def get_list_avg(list):
+    return sum(list)/len(list)
+
+model_pkl = torch.load('models/densenet201_distracted.pkl')
+video = cv2.VideoCapture('data/distracted.mp4')
 capture, image = video.read()
+times = []
 
 
 while video.isOpened():
@@ -27,7 +34,10 @@ while video.isOpened():
     if ret is True:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         resized = cv2.resize(gray, (640, 480))
+        start_time = current_milli_time()
         result = model_pkl.predict(resized)
+        end_time = current_milli_time()
+        times.append(end_time - start_time)
         print(states[result[0]])
         cv2.imshow('Frame', gray)
         if cv2.waitKey(1) and 0xFF == ord ('q'):
@@ -36,4 +46,10 @@ while video.isOpened():
         break
 video.release()
 cv2.destroyAllWindows()
+
+print(times)
+print(get_list_avg(times))
+print(len(times))
+#resnet_50 - 75.40517241379311
+#densenet_201 - 119.2758
 
